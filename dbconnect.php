25670844bc0e16a50d6dfa1e3edc97f9954c19f1
smishra2841 <mishrasaurabh@live.com>
2016-02-09 22:17:15 -0500

    cleardb trial3

--- dbconnect.php
+++ dbconnect.php
@@ -2,15 +2,12 @@
 
 
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["us-cdbr-iron-east-03.cleardb.net"];
-$username = $url["bb54b7a6852acd"];
-$password = $url["e4849472"];
-$db = substr($url["heroku_cf76f2266f6f3d1"], 1);
+
+$server = "us-cdbr-iron-east-03.cleardb.net";
+$username = "bb54b7a6852acd";
+$password = "e4849472";
+$db ="heroku_cf76f2266f6f3d1";
 
 $conn = new mysqli($server, $username, $password, $db);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
 ?>
