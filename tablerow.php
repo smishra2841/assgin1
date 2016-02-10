88a069e8f382f5eb1d42a5385fd037121cde3ed6
smishra2841 <mishrasaurabh@live.com>
2016-02-10 11:06:00 -0500

    first commit

--- tablerow.php
+++ tablerow.php
@@ -5,8 +5,10 @@
 	 	echo "<tr>";
 
 		$first_day = mktime(0,0,0,$month, 1, $year) ;
+		echo "$first_day";
 		$name_of_day = date('w', $first_day) ; //get the day of the weeek
 		//caluting blank days for the month for diaplay 
+		echo "$name_of_day";
 		switch($name_of_day){   
 			case "0": $blank = 0; 
 			break;   
