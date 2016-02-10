7962c7e9f5a5d03c401f04f86941a3873483e7f3
smishra2841 <mishrasaurabh@live.com>
2016-02-09 22:07:46 -0500

    cleardb trial3

--- dbconnect.php
+++ dbconnect.php
@@ -10,4 +10,7 @@ $password = $url["e4849472"];
 $db = substr($url["heroku_cf76f2266f6f3d1"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 ?>
