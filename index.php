ede338dc8b05990f6ae069427ce8118b5c6555fd
smishra2841 <mishrasaurabh@live.com>
2016-02-10 14:23:21 -0500

    fb login

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include 'calander2.php' ?>
 <?php
 session_start();
-require_once __DIR__ . '/assgin1/src/Facebook/autoload.php';
+require_once  'autoload.php';
 $fb = new Facebook\Facebook([
   'app_id' => '763789147084760',
   'app_secret' => '7199d3ede5818b5e50c16eef50f027bf',
